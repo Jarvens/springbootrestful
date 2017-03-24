bd9d71a88e9a86757a0b4f48cc641f963ce08fab
Jarven <769558579@qq.com>
2017-03-24 13:39:02 +0800

    RESTful接口

---
+++ src/main/java/com/kunlun/Application.java
@@ -0,0 +1,17 @@
+package com.kunlun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序主入口
+ * Created by kunlun on 2017/3/24.
+ */
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
